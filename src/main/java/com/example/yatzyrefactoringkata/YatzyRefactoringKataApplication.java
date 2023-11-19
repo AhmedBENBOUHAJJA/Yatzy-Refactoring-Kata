8cb483209b4dc623d2137cf852d26ce95af96a9c
Azus <a.ben.bouhajja@gmail.com>
2023-11-19 18:45:20 +0100

    init project

---
+++ src/main/java/com/example/yatzyrefactoringkata/YatzyRefactoringKataApplication.java
@@ -0,0 +1,13 @@
+package com.example.yatzyrefactoringkata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YatzyRefactoringKataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YatzyRefactoringKataApplication.class, args);
+    }
+
+}
